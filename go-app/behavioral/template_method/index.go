0f547c59faebf6c58566c0f3f86bd70317804cc1
pochon-tech-pro <pochon.tech.0519@gmail.com>
2022-01-31 12:22:35 +0900

    feat: CMD引数で特定のPackageの処理を呼び出せるようにする

---
+++ go-app/behavioral/template_method/index.go
@@ -0,0 +1,7 @@
+package template_method
+
+import "fmt"
+
+func Run() {
+	fmt.Println("hello template_method")
+}
--- go-app/main.go
+++ go-app/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"go-app/behavioral/template_method"
+)
 
 func main() {
 	fmt.Println("go run main.go ")
+
+	flag.Parse() // ここで引数解析
+
+	if flag.Arg(0) == "template_method" { //  go run main.go template_method
+		template_method.Run()
+	}
 }
